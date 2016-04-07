a69dac81dbab31dedc50e74b163a52e9e45eabd3
William Burke <williamburke@williamdburke.com>
2016-04-06 22:19:09 -0500

    Add project files.

---
+++ NuHue/App.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+using Windows.Security.ExchangeActiveSyncProvisioning;
+
+namespace NuHue
+{
+    public partial class App : Application
+    {
+        //apiKey is used to pass the registered key inside the app.
+        public static string apiKey { get; set; } = "";
+        public string appKey;
+        public static ILocalHueClient client;
+
+        /// <summary>
+        /// Try to connect to the bridge and register the application client
+        /// </summary>
+        public async void BridgeLocation()
+        {
+            //Locate the bridge, pull the current device info and get the bridge IP
+            IBridgeLocator locator = new HttpBridgeLocator();
+            var device = DeviceInfo();
+            IEnumerable<string> bridgeIPs = await locator.LocateBridgesAsync(TimeSpan.FromSeconds(5));
+            //Register the client and pull the application key
+            client = ClientCheck(bridgeIPs.First()).Result;
+        }
+
+        public async Task<LocalHueClient> ClientCheck(string bridgeIP)
+        {
+            client = new LocalHueClient(bridgeIP);
+            try
+            {
+                client.Initialize(apiKey);
+            }
+            catch (Exception e)
+            {
+                var a = e.Message;
+                //TODO: Catch exception and prompt user to press the link button and register app with bridge
+                appKey = await client.RegisterAsync("NuHue", apiKey);
+            }
+            return client as LocalHueClient;
+        }
+
+        public string DeviceInfo()
+        {
+            // get the device manufacturer and model name
+            EasClientDeviceInformation eas = new EasClientDeviceInformation();
+            var DeviceManufacturer = eas.SystemManufacturer;
+            var DeviceModel = eas.SystemProductName;
+            return DeviceModel;
+        }
+
+    }
+}
---
+++ NuHue/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Q42.HueApi;
+using Q42.HueApi.Interfaces;
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace NuHue
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void onButton_Click(object sender, RoutedEventArgs e)
+        {
+            var command = new LightCommand();
+            command.On = true;
+            App.client.SendCommandAsync(command);
+        }
+
+        private void offButton_Click(object sender, RoutedEventArgs e)
+        {
+            var command = new LightCommand();
+            command.TurnOff();
+            App.client.SendCommandAsync(command);
+        }
+    }
+}
